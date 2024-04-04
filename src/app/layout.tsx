4006705c79b83311f4cdd46576d196c81f33da21
Nicolas Demontrond <demontrond.nicolas@gmail.com>
2024-04-04 12:23:13 +0200

    modified header into a component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,12 +17,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                <header
-                    style={{ backgroundColor: "lightblue", padding: "1rem" }}
-                >
-                    <p>Header</p>
-                </header>
+            <body
+                className={`${inter.className} bg-gray-50 text-gray-950 relative`}
+            >
+                <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+                <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+
+                <Header />
                 {children}
                 <footer
                     style={{ backgroundColor: "ghostwhite", padding: "1rem" }}
