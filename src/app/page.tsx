e8b8495636052044158f2d158fa9fd480f32d247
Nicolas Demontrond <demontrond.nicolas@gmail.com>
2024-04-06 16:08:13 +0200

    added a section divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 // src/app/page.tsx
 
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 import React from "react";
 
 export default function Home() {
     return (
         <main className="flex flex-col items-center px-4">
             <Intro />
+            <SectionDivider />
         </main>
     );
 }
