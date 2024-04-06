5cb8f26fa8e3a224f1bd7c329740c6a673845bb3
Nicolas Demontrond <demontrond.nicolas@gmail.com>
2024-04-06 03:19:00 +0200

    added a short intro

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -46,6 +46,18 @@ export default function Intro() {
                     </motion.span>
                 </div>
             </div>
+
+            <motion.h1
+                className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl pt-[5rem]"
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+            >
+                <span className="font-bold">Hello, I&apos;m Nicolas.</span>{" "}
+                I&apos;m a{" "}
+                <span className="font-bold">junior developer</span>. I
+                enjoy building <span className="italic">sites & apps</span>. My
+                focus is <span className="underline">React (Next.js)</span>.
+            </motion.h1>
         </section>
     );
 }
