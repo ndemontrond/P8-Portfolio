c7911e704901da976b5b92ebc416e9965eb2fda2
Nicolas Demontrond <demontrond.nicolas@gmail.com>
2024-04-08 16:53:54 +0200

    added persistent theme setting

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
 
 type Theme = "light" | "dark";
@@ -11,22 +11,29 @@ export default function ThemeSwitch() {
     const toggleTheme = () => {
         if (theme === "light") {
             setTheme("dark");
+            window.localStorage.setItem("theme", "dark");
         } else {
             setTheme("light");
+            window.localStorage.setItem("theme", "light");
         }
     };
+
+    useEffect(() => {
+        const storedTheme = window.localStorage.getItem(
+            "theme"
+        ) as Theme | null;
+
+        if (storedTheme) {
+            setTheme(storedTheme);
+        }
+    }, []);
 
     return (
         <button
             className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-115 active:scale-105 transition-all"
             onClick={toggleTheme}
         >
-            {
-                theme === "light" ? (
-            <BsSun /> ) : (
-                <BsMoon />
-            )
-            }
+            {theme === "light" ? <BsSun /> : <BsMoon />}
         </button>
     );
 }
